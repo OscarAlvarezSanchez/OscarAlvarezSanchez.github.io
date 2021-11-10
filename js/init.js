69826135a6eb5884aaacfbcd39c4fbdc6763c77e
OscarAlvarezSanchez <94078326+OscarAlvarezSanchez@users.noreply.github.com>
2021-11-10 14:08:44 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyDFhpVpFsz8jniG-Vl8-ZLXYtPMqrDmfVs",
+  authDomain: "moas-4be8a.firebaseapp.com",
+  projectId: "moas-4be8a",
+  storageBucket: "moas-4be8a.appspot.com",
+  messagingSenderId: "160256643574",
+  appId: "1:160256643574:web:d41163a1dcc399f49977d7",
+  measurementId: "G-X43MC7VBJN"
+    });
